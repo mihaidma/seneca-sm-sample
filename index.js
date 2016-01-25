4ee6eca4b1039453a4b87c8269827b8ccc9e01c5
Mihai Dima <mihaizn@gmail.com>
2016-01-25 15:39:48 +0200

    state changing

--- index.js
+++ index.js
@@ -1,42 +1,52 @@
 
 var Async = require('async')
 var seneca = require('seneca')()
-seneca.use(require('seneca-sm'))
+seneca.use('seneca-sm')
 var config = require('./smconfig.js').config
 
 seneca.add('role:content, context:document, cmd: triage', function(err, respond) {
-  respond(null, {state: 'triage state'})
+  respond(null, {command: 'execute command: role:content, context:document, cmd: triage'})
 })
-.add("role:'content', context:'document', cmd: 'review'", function(err, respond) {
-  respond(null, {state: 'review state'})
+.add('role:content, context:document, cmd: review', function(err, respond) {
+  respond(null, {command: 'execute command: role:content, context:document, cmd: review'})
 })
-.add("role:'content', context:'document', cmd: 'approve'", function(err, respond) {
-  respond(null, {state: 'approve state'})
+.add('role:content, context:document, cmd: approve', function(err, respond) {
+  respond(null, {command: 'execute command: role:content, context:document, cmd: approve'})
 })
-.add("role:'content', context:'document', cmd: 'schedule'", function(err, respond) {
-  respond(null, {state: 'schedule state'})
+.add('role:content, context:document, cmd: schedule', function(err, respond) {
+  respond(null, {command: 'execute command: role:content, context:document, cmd: schedule'})
 })
-.add("role:'content', context:'document', cmd: 'approve_final'", function(err, respond) {
-  respond(null, {state: 'approve_final state'})
+.add('role:content, context:document, cmd: approve_final', function(err, respond) {
+  respond(null, {command: 'execute command: role:content, context:document, cmd: approve_final'})
 })
 
 Async.series({
   create_instance: function (callback) {
     seneca.act("role: 'sm', create: 'instance'", config, function (err, context) {
+      console.log('created the state machine:', config.name)
       callback(err)
     })
   },
   verify_state: function (callback) {
     seneca.act('role:' + config.name + ', get:context', function (err, context) {
-    //   console.log('context', context)
+      console.log('current state: ', context.current_status)
+      callback(err)
+    })
+  },
+  triage_document: function (callback) {
+    console.log('change state to triage')
+    seneca.act('role:' + config.name + ', cmd: triage', function(err, context) {
+      console.log('state changed')
+      callback(err)
+    })
+  },
+  verify_triage_state: function (callback) {
+    seneca.act('role:' + config.name + ', get:context', function (err, context) {
+      console.log('current state: ', context.current_status)
       callback(err)
     })
   }
 },
 function (err, results) {
-  console.log(err, results)
+//   console.log(err, results)
 })
-
-// seneca.act('role:content, context: document, cmd: triage', function(err, result) {
-//   console.log(result)
-// })
